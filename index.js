381214d4ae1f722aa29c63ff3d0f7c53914403be
朕与将军解战袍 <1393323447@qq.com>
2023-09-27 18:29:24 +0800

    v0.1

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require("@actions/core");
+const axios = require('axios')
+
+try {
+    const group = core.getInput("group");
+    const repo = core.getInput("repo");
+    const match = core.getInput("match");
+
+    const api_url = `https://api.github.com/repos/${group}/${repo}/releases/latest`;
+    const re = new RegExp(match);
+
+    axios.get(api_url).then((res) => {
+        for (let asset of res.data.assets) {
+            if (re.test(asset)) {
+                core.setOutput("download_link", asset.browser_download_url);
+            }
+        }
+    });
+} catch (error) {
+    core.setFailed(error.message);
+}
