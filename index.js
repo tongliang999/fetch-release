8d85b0e0e45a1defc65a12b3f9d9dff1dafc1946
朕与将军解战袍 <1393323447@qq.com>
2023-09-28 10:22:09 +0800

    update

--- index.js
+++ index.js
@@ -4,15 +4,19 @@ const axios = require('axios');
 try {
     const group = core.getInput("group");
     const repo = core.getInput("repo");
+    const tag = core.getInput("tag");
     const match = core.getInput("match");
 
-    const api_url = `https://api.github.com/repos/${group}/${repo}/releases/latest`;
+    const api_url = tag === '' ? `https://api.github.com/repos/${group}/${repo}/releases/latest`
+                               : `https://api.github.com/repos/${group}/${repo}/releases/tags/${tag}`;
     const re = new RegExp(match);
 
     axios.get(api_url).then((res) => {
-        for (let asset of res.data.assets) {
+        const release = res.data;
+        for (let asset of release.assets) {
             if (re.test(asset.name)) {
                 core.setOutput("download-link", asset.browser_download_url);
+                core.setOutput("release-tag", release.tag);
                 console.log(`Found matched release download link: ${asset.browser_download_url}`);
                 return;
             }
