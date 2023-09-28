981d6ebf2793cc4def4a5711cef928c2e7c3cae4
朕与将军解战袍 <1393323447@qq.com>
2023-09-28 11:20:21 +0800

    add a retry loop

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
-const axios = require('axios');
+const axios = require("axios");
+const sleep = require("sleep");
 
 try {
     const group = core.getInput("group");
@@ -11,19 +12,28 @@ try {
                                : `https://api.github.com/repos/${group}/${repo}/releases/tags/${tag}`;
     const re = new RegExp(match);
 
-    axios.get(api_url).then((res) => {
-        const release = res.data;
-        for (let asset of release.assets) {
-            console.log(`checking ${asset.name}\n`);
-            if (re.test(asset.name)) {
-                core.setOutput("download-link", asset.browser_download_url);
-                core.setOutput("release-tag", release.tag);
-                console.log(`Found matched release download link: ${asset.browser_download_url}`);
-                return;
-            }
+    let getReply = false;
+    for (let i = 0; i < 5 && !getReply; i++) {
+        try {
+            axios.get(api_url).then((res) => {
+                getReply = true;
+                const release = res.data;
+                for (let asset of release.assets) {
+                    console.log(`checking ${asset.name}\n`);
+                    if (re.test(asset.name)) {
+                        core.setOutput("download-link", asset.browser_download_url);
+                        core.setOutput("release-tag", release.tag);
+                        console.log(`Found matched release download link: ${asset.browser_download_url}`);
+                        return;
+                    }
+                }
+                core.setFailed(`no matching release (re = ${match})`);
+            });
+        } catch (err) {
+            console.log("Retry after 5s\n");
+            sleep.sleep(5);
         }
-        core.setFailed(`no matching release (re = ${match})`);
-    });
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
